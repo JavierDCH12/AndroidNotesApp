96f7a6d79445ccc5ecf07623b5af1c677368d081
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-11-19 23:03:39 +0100

    note class and viewmodel add

---
+++ app/src/main/java/com/example/androidnotesapp/model/Note.java
@@ -0,0 +1,59 @@
+package com.example.androidnotesapp.model;
+
+public class Note {
+
+    private int id;
+    private String title;
+    private String content;
+    private String category;
+
+    public Note(int id, String title, String content, String category) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", category='" + category + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/androidnotesapp/model/NoteViewModel.java
@@ -0,0 +1,43 @@
+package com.example.androidnotesapp.model;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteViewModel extends ViewModel {
+
+    private final MutableLiveData<Note> selectedNote = new MutableLiveData<>();
+    private final MutableLiveData<List<Note>> note_list = new MutableLiveData<>(new ArrayList<>());
+
+
+
+    public void selectNote(Note note){
+        selectedNote.setValue(note);
+    }
+
+    public MutableLiveData<Note> getSelectedNote(){
+        return selectedNote;
+    }
+
+    public MutableLiveData<List<Note>> getNoteList(){
+        return note_list;
+    }
+
+    public void addNote(Note note){
+        List<Note> currentList = new ArrayList<>(note_list.getValue());
+        currentList.add(note);
+        note_list.setValue(currentList);
+    }
+
+    public void deleteNote(Note note){
+        List<Note> currentList = new ArrayList<>(note_list.getValue());
+        currentList.remove(note);
+        note_list.setValue(currentList);
+    }
+
+
+
+}
