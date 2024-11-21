9e61858039eef364ad232a4044f08e03c1dc156e
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-11-21 20:59:56 +0100

    fragment gallery, main top and bottom bard added

--- app/src/main/java/com/example/androidnotesapp/MainActivity.java
+++ app/src/main/java/com/example/androidnotesapp/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.androidnotesapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.androidnotesapp.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +27,30 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        //TOOLBAR
+        setSupportActionBar(binding.toolbar);
+
+
+        //BOTTOM BAR
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(binding.navHost.getId());
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomMenu, navController);
+
+        binding.bottomMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.fragmentGalleryNotes){
+                   // binding.toolbar.setTitle(getString(R.string.toolbar_gallery_title));
+                    //binding.toolbar.setSubtitle(getString(R.string.toolbar_gallery_subtitle));
+                   navController.navigate(R.id.fragmentGalleryNotes);
+
+
+                }
+                return true;
+            }
+        });
 
 
 
